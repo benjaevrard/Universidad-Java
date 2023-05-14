181f0f764738642f43c42b9e95fca0e7b833e6ab
Cuent <Cuent@DESKTOP-8QG186A>
2023-05-14 17:28:30 -0400

    Carga Inicial

---
+++ src/main/java/com/mycompany/prueba_java/DAO_prueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba_java;
+
+/**
+ *
+ * @author Cuent
+ */
+public class DAO_prueba {
+    
+}
---
+++ src/main/java/com/mycompany/prueba_java/Perro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba_java;
+
+/**
+ *
+ * @author Cuent
+ */
+public class Perro {
+    private int id_perro;
+    private String raza_perro;
+}
+
